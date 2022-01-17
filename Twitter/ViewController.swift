66a4428bdff5a488ac209bb9c421fc1cac747ac6
Marco Kang <kmnkit@gmail.com>
2022-01-17 20:28:14 +0900

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Sungwoong Kang on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
